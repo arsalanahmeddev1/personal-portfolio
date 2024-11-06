aae2b168a26a81c7e630c9f7b2d837a1a6da9c82
arsalanahmed <arsalanahmeddev1@gmail.com>
2024-11-06 06:24:03 +0500

    some changes

---
+++ src/content/workContent.js
@@ -0,0 +1,16 @@
+export const workTitle = 'My Work';
+
+import workThumbnail1 from '../images/work-thumnail-01.webp';
+import workThumbnail2 from '../images/work-thumnail-01.webp';
+import workThumbnail3 from '../images/work-thumnail-01.webp';
+import workThumbnail4 from '../images/work-thumnail-01.webp';
+import workThumbnail5 from '../images/work-thumnail-01.webp';
+import workThumbnail6 from '../images/work-thumnail-01.webp';
+export const myWork = [
+  {id: 1, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail1  },
+  {id: 2, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail2 },
+  {id: 3, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail3 },
+  {id: 4, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail4 },
+  {id: 5, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail5 },
+  {id: 6, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail6 },
+]
