8344faf2dc9f465c84f58e526ff472f85b5c5603
Arsalan Ahmed <arsalanahmed@pop-os.www.mtlinkwifi.com>
2025-01-22 07:48:34 +0500

    some changes in every sec

--- src/content/workContent.js
+++ src/content/workContent.js
@@ -1,16 +1,16 @@
 export const workTitle = 'My Work';
 
-import workThumbnail1 from '../images/diginsy.png';
-import workThumbnail2 from '../images/work-thumnail-01.webp';
-import workThumbnail3 from '../images/work-thumnail-01.webp';
-import workThumbnail4 from '../images/work-thumnail-01.webp';
-import workThumbnail5 from '../images/work-thumnail-01.webp';
+import workThumbnail1 from '../images/diginsy-updated.png';
+import workThumbnail2 from '../images/finitive.png';
+import workThumbnail3 from '../images/diligenttek.png';
+import workThumbnail4 from '../images/designfoxy.png';
+import workThumbnail5 from '../images/designsketchers.png';
 import workThumbnail6 from '../images/work-thumnail-01.webp';
 export const myWork = [
-  {id: 1, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail1  },
-  {id: 2, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail2 },
-  {id: 3, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail3 },
-  {id: 4, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail4 },
-  {id: 5, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail5 },
+  {id: 1, name: 'Diginsy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail1, link: 'https://diginsy.com/' },
+  {id: 2, name: 'Logo Finitive', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail2, link: 'https://www.logofinitive.com/' },
+  {id: 3, name: 'Diligenttek', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail3, link: 'https://www.diligenttek.com/' },
+  {id: 4, name: 'Design Foxy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail4, link: 'https://www.designfoxy.com/' },
+  {id: 5, name: 'Design Sketchers', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail5, link: 'https://www.designsketchers.com/' },
   {id: 6, name: 'name here', type: 'ecommerce', alt: 'img-alt', year: 2024, img: workThumbnail6 },
 ]
