a6df9c8bd902e2911bb62f1f015d189053f633b7
Arsalan Ahmad <arsalanwebdev@gmail.com>
2025-05-04 12:05:41 +0500

    some change in sections

--- src/content/aboutContent.js
+++ src/content/aboutContent.js
@@ -1,5 +1,5 @@
 export const aboutTitle = 'Who I Am';
-export const aboutPara = "Hello! I’m Arsalan Ahmed, a web developer dedicated to crafting seamless and impactful digital experiences. With over 3 years in the industry, I bring expertise in building websites that are visually appealing, high-performing, and user-focused.";
+export const aboutPara = "Hello! I’m Arsalan Ahmed, a web developer dedicated to crafting seamless and impactful digital experiences. With over 4 years in the industry, I bring expertise in building websites that are visually appealing, high-performing, and user-focused.";
 export const aboutPara2 = "My skill set spans HTML, CSS, JavaScript, React, Tailwind CSS, and Astro, along with various JavaScript libraries that allow me to create modern, responsive, and efficient websites. I'm passionate about staying up-to-date with the latest tools and best practices, ensuring each project not only meets but exceeds expectations.";
 export const skill1 =  'html';
 export const skill2 =  'Css';
--- src/content/contactFormContent.js
+++ src/content/contactFormContent.js
@@ -4,7 +4,7 @@ export const submitBtn = 'Submit'
 export const formFields = [
   {type: 'text', name: 'name', placeholder: 'Your Name', class: 'w-full bg-transparent px-[10px] py-[20px] h-[65px] mb-[10px] sm:mb-[40px] text-white border-b border-white placeholder:text-white focus-within:outline-none text-[14px] '},
   {type: 'email', name: 'email', placeholder: 'Your Email', class: 'w-full bg-transparent px-[10px] py-[20px] h-[65px] mb-[10px] sm:mb-[40px] text-white border-b border-white placeholder:text-white focus-within:outline-none text-[14px] focus:border-color-red'},
-  {type: 'number', name: 'phone',  placeholder: 'Your Phone', class: 'w-full bg-transparent px-[10px] py-[20px] h-[65px] mb-[10px] sm:mb-[40px] text-white border-b border-white placeholder:text-white focus-within:outline-none text-[14px] focus:border-color-red'},
+  {type: 'tel', name: 'phone',  placeholder: 'Your Phone', class: 'w-full bg-transparent px-[10px] py-[20px] h-[65px] mb-[10px] sm:mb-[40px] text-white border-b border-white placeholder:text-white focus-within:outline-none text-[14px] focus:border-color-red'},
   {type: 'textarea', name: 'message', placeholder: 'Your Message', class: 'w-full bg-transparent px-[10px] py-[20px] h-[65px] mb-[10px] sm:mb-[40px] text-white border-b border-white placeholder:text-white focus-within:outline-none text-[14px] focus:border-color-red resize-none'},
   {type: 'submit', name: 'submit', text: 'submit', class: 'bg-theme-gradiant max-w-[max-content] flex justify-center mx-auto text-white py-[12px] px-[70px] rounded-[60px]'}
 ]
