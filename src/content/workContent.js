5930c667d6be6a9bd33838226b34be975c67a6eb
unknown <production8411@gmail.com>
2025-05-08 20:30:33 +0500

    added one project

--- src/content/workContent.js
+++ src/content/workContent.js
@@ -1,5 +1,6 @@
 export const workTitle = 'My Work';
 
+import workThumbnail7 from '../images/khryslis.png';
 import workThumbnail1 from '../images/diginsy-updated.png';
 import workThumbnail2 from '../images/finitive.png';
 import workThumbnail3 from '../images/diligenttek.png';
@@ -7,6 +8,7 @@ import workThumbnail4 from '../images/designfoxy.png';
 import workThumbnail5 from '../images/designsketchers.png';
 import workThumbnail6 from '../images/goves.png';
 export const myWork = [
+  {id: 1, name: 'Social Media React', type: 'social media', alt: 'img-alt', year: 2024, img: workThumbnail7, link: 'https://khrylis-social.vercel.app/' },
   {id: 1, name: 'Diginsy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail1, link: 'https://diginsy.com/' },
   {id: 2, name: 'Logo Finitive', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail2, link: 'https://www.logofinitive.com/' },
   {id: 3, name: 'Diligenttek', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail3, link: 'https://www.diligenttek.com/' },
