b1e372f0aea8f8936448396e9d16af52921e8ee6
Arsalan Ahmad <arsalanwebdev@gmail.com>
2025-06-20 09:40:39 +0500

    added new project

--- src/content/workContent.js
+++ src/content/workContent.js
@@ -1,4 +1,5 @@
 
+import workThumbnail9 from '../images/storie.jpeg';
 import workThumbnail7 from '../images/khryslis.png';
 import workThumbnail8 from '../images/one-car.png';
 import workThumbnail1 from '../images/diginsy-updated.png';
@@ -9,9 +10,10 @@ import workThumbnail5 from '../images/designsketchers.png';
 import workThumbnail6 from '../images/goves.png';
 export const workTitle = 'My Work';
 export const myWork = [
-  {id: 1, name: 'Car Selling React', type: 'website', alt: 'img-alt', year: 2025, img: workThumbnail8, link: 'https://one-off-autos-beta.vercel.app/' },
-  {id: 1, name: 'Social Media React', type: 'social media', alt: 'img-alt', year: 2025, img: workThumbnail7, link: 'https://khrylis-social.vercel.app/' },
-  {id: 1, name: 'Diginsy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail1, link: 'https://diginsy.com/' },
+  {id: 1, name: 'Storie Vault', type: 'website', alt: 'img-alt', year: 2025, img: workThumbnail9, link: 'https://author.sitestaginglink.com/' },
+  {id: 9, name: 'Car Selling React', type: 'website', alt: 'img-alt', year: 2025, img: workThumbnail8, link: 'https://one-off-autos-beta.vercel.app/' },
+  {id: 8, name: 'Social Media React', type: 'social media', alt: 'img-alt', year: 2025, img: workThumbnail7, link: 'https://khrylis-social.vercel.app/' },
+  {id: 7, name: 'Diginsy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail1, link: 'https://diginsy.com/' },
   {id: 2, name: 'Logo Finitive', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail2, link: 'https://www.logofinitive.com/' },
   {id: 3, name: 'Diligenttek', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail3, link: 'https://www.diligenttek.com/' },
   {id: 4, name: 'Design Foxy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail4, link: 'https://www.designfoxy.com/' },
