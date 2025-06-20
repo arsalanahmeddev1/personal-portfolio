eb85bc2411caf00be3e4645503fed2a972acce1b
Arsalan Ahmad <arsalanwebdev@gmail.com>
2025-06-20 09:03:46 +0500

    minor changes

--- src/content/bannerContent.js
+++ src/content/bannerContent.js
@@ -1,5 +1,5 @@
 export const bannerHeading = 'Im Arsalan Ahmed,'
-export const bannerHeadingSpan = 'Frontend Developer Based In Pakistan.'
+export const bannerHeadingSpan = 'Frontend Developer Based In Pakistan. testing frontend'
 export const bannerPara = 'I am a frontend developer from Pakistan with 4 years of experience in multiple companies.';
 export const bannerleftBtn = 'Contact Me';
 export const bannerrightBtn = 'My Resume ';
