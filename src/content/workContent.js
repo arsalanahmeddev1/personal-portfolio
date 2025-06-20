a5e8883f8abb9498593c1f973f7a228342a5ed29
Arsalan Ahmad <arsalanwebdev@gmail.com>
2025-06-20 09:32:53 +0500

    links changes

--- src/content/workContent.js
+++ src/content/workContent.js
@@ -9,8 +9,8 @@ import workThumbnail5 from '../images/designsketchers.png';
 import workThumbnail6 from '../images/goves.png';
 export const workTitle = 'My Work';
 export const myWork = [
-  {id: 1, name: 'Car Selling React', type: 'website', alt: 'img-alt', year: 2025, img: workThumbnail7, link: 'https://khrylis-social.vercel.app/' },
-  {id: 1, name: 'Social Media React', type: 'social media', alt: 'img-alt', year: 2025, img: workThumbnail8, link: 'https://one-off-autos-beta.vercel.app/' },
+  {id: 1, name: 'Car Selling React', type: 'website', alt: 'img-alt', year: 2025, img: workThumbnail8, link: 'https://one-off-autos-beta.vercel.app/' },
+  {id: 1, name: 'Social Media React', type: 'social media', alt: 'img-alt', year: 2025, img: workThumbnail7, link: 'https://khrylis-social.vercel.app/' },
   {id: 1, name: 'Diginsy', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail1, link: 'https://diginsy.com/' },
   {id: 2, name: 'Logo Finitive', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail2, link: 'https://www.logofinitive.com/' },
   {id: 3, name: 'Diligenttek', type: 'web brand', alt: 'img-alt', year: 2024, img: workThumbnail3, link: 'https://www.diligenttek.com/' },
